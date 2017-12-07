f48e55440fb4ec355abf74cf4fe54e8661e194ec
SamLawsz <luo33800530>
2017-12-07 19:03:34 +0800

    19:03随便上传一下

--- letao/letao/public/manage_my/js/login.js
+++ letao/letao/public/manage_my/js/login.js
@@ -28,7 +28,7 @@ $(function () {
                     },
                     //长度校验
                     stringLength: {
-                        min: 6,
+                        min: 2,
                         max: 24,
                         message: '用户名长度必须在6到30之间'
                     },
@@ -42,12 +42,34 @@ $(function () {
                     },
                     //长度校验
                     stringLength: {
-                        min: 6,
+                        min: 2,
                         max: 24,
                         message: '密码长度必须在6到30之间'
                     },
                 }
             }
         }
-    })
+    }).on('success.form.bv', function (e) {
+        e.preventDefault();
+        //使用ajax提交逻辑
+        $.ajax({
+            url: "/employee/employeeLogin",
+            data: $("form").serialize(),
+            type: 'post',
+            success: function (backData) {
+                console.log(backData)
+                if(backData==1){
+                    window.location.href=("./index.html")
+                    console.log(backData)
+                } else {
+                    console.log(backData)
+                    if (backData==1000){
+                        console.log(backData)
+                    } else if (backData == 1000){
+                        console.log(backData)
+                    }
+                }
+            }
+        })
+    });
 })
