7869858ba7315f18e902821b3b2b655cce411e78
SamLawsz <luo33800530>
2017-12-07 21:23:24 +0800

    今天的所有代码

--- letao/letao/public/manage_my/js/login.js
+++ letao/letao/public/manage_my/js/login.js
@@ -1,16 +1,5 @@
 $(function () {
-    // ajax向服务器发送数据
-    // $('button[type=submit]').click(function (event) {
-    //     event.preventDefault();
-    //     $.ajax({
-    //         url: "/employee/employeeLogin",
-    //         data: $("form").serialize(),
-    //         type: 'post',
-    //         success: function (backData) {
-    //             console.log(backData);
-    //         }
-    //     })
-    // })
+
     // 表单验证
     $("form").bootstrapValidator({
         feedbackIcons: {
@@ -26,12 +15,16 @@ $(function () {
                     notEmpty: {
                         message: '用户名不能为空'
                     },
-                    //长度校验
+
+                    // 长度校验
                     stringLength: {
                         min: 2,
                         max: 24,
                         message: '用户名长度必须在6到30之间'
                     },
+                    callback: {
+                        message: '你妹的，会不会玩，账号都写错了'
+                    }
                 }
             },
             password: {
@@ -46,30 +39,55 @@ $(function () {
                         max: 24,
                         message: '密码长度必须在6到30之间'
                     },
+                    callback: {
+                        message: '回家玩屎去吧，密码错了,还搞个毛~！'
+                    }
                 }
             }
         }
     }).on('success.form.bv', function (e) {
         e.preventDefault();
         //使用ajax提交逻辑
+        // 开启进度条
+
+        NProgress.start();
         $.ajax({
             url: "/employee/employeeLogin",
             data: $("form").serialize(),
             type: 'post',
             success: function (backData) {
-                console.log(backData)
-                if(backData==1){
-                    window.location.href=("./index.html")
-                    console.log(backData)
+                // console.log(backData)
+                if (backData.success == 1) {
+                    window.location.href = "./index.html";
                 } else {
+                    var validator = $("form").data('bootstrapValidator')
                     console.log(backData)
-                    if (backData==1000){
-                        console.log(backData)
-                    } else if (backData == 1000){
-                        console.log(backData)
+                    if (backData.error == 1000) {
+                        // 账号错误
+                        validator.updateStatus('username', 'INVALID', 'callback');
+                    } else if (backData.error == 1001) {
+                        // 密码错误
+                        validator.updateStatus('password', 'INVALID', 'callback');
                     }
                 }
+
+                // 进度条处理
+                setTimeout(function () {
+                    NProgress.done();
+                }, 1000)
             }
         })
     });
+
+    // 3.为重置表单绑定的点击事件
+    $('button[type=reset]').click(function(){
+        // 获取验证插件对象
+        var validator = $('form').data('bootstrapValidator');
+        // 获取表单校验实例
+        validator.resetForm();
+        
+    })
+
+
+
 })
